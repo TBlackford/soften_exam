296de2ba412005e07c9c2ededebb1e551f5aad94
TBlackford <PAINTA1@student.op.ac.nz>
2017-06-16 15:30:04 +1200

    Heroku test

--- flaskr/flaskr/__init__.py
+++ flaskr/flaskr/__init__.py
@@ -3,4 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object(__name__) # load config from this file , flaskr.py
 
-import flaskr.views
+import flaskr.flaskr.views
--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -52,5 +52,3 @@ def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
-
-
